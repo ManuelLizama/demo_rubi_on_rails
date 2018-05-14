9c8d447f2870b34ca1a26e1b6e4b093a3be2944e
manuellizama <manuellizamaac@outlook.cl>
2018-05-13 23:39:43 -0400

    modificaciones de estilo, controlador y vista index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,21 @@
 class ArticlesController < ApplicationController
 	#get /articles
-	def index	
+	def index
+		@articles = Article.all
+
+	end
+	#get /articles/:id
+	def show
+		@article = Article.find(params[:id])
+	end
+	#get /articles/new
+	def new
+		@article = Article.new
+	end
+	#post /articles
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article.save
+		redirect_to @article
 	end
 end
